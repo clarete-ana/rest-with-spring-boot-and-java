eeec137706e11569f7d164e30050973b2835e009
Clarete-ana <anaclaraclarete2@gmail.com>
2025-07-22 17:52:25 -0300

    Criados metodos put, post e delete

--- rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -4,10 +4,9 @@ import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -23,4 +22,38 @@ public class PersonController {
     public Person findById(@PathVariable("id") String id){
         return service.findById(id);
     }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person){
+        return service.create(person);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person){
+        return service.update(person);
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void deleteById(@PathVariable("id") String id){
+        service.delete(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findById(){
+        return service.findAll();
+    }
+
 }
--- rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/services/PersonServices.java
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.erudio.services;
 import br.com.erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,16 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll(){
+        logger.info("Finding all people!");
+        List<Person> people = new ArrayList<Person>();
+        for (int i = 0; i <8; i++){
+            Person person = mockPerson(i);
+            people.add(person);
+        }
+        return people;
+    }
 
     public Person findById(String id){
         logger.info("Finding one Person!");
@@ -23,6 +35,31 @@ public class PersonServices {
         person.setAddress("São Paulo - SP");
         person.setGender("Female");
 
+        return person;
+    }
+
+    public Person create(Person person){
+        logger.info("Creating one person!");
+        return person;
+    }
+
+    public Person update(Person person){
+        logger.info("Updating one person!");
+        return person;
+    }
+
+    public void delete(String id){
+        logger.info("Deleting one person!");
+    }
+
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Some Address in Brazil");
+        person.setGender("Male");
         return person;
     }
 }
