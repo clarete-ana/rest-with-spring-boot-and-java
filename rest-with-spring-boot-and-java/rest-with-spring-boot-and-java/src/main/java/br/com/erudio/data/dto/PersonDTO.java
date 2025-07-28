099dec8a9d81d024efa70a8047eb79e931757948
Clarete-ana <anaclaraclarete2@gmail.com>
2025-07-28 15:50:26 -0300

    DTO

---
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/data/dto/PersonDTO.java
@@ -0,0 +1,74 @@
+package br.com.erudio.data.dto;
+
+import br.com.erudio.model.Person;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonDTO implements Serializable{
+
+        private static final long serialVersionUID = 1L;
+        private Long id;
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String gender;
+
+        public PersonDTO() {}
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof br.com.erudio.model.Person)) return false;
+            br.com.erudio.model.Person person = (br.com.erudio.model.Person) o;
+            return getId().equals(person.getId()) && getFirstName().equals(person.getFirstName()) && getLastName().equals(person.getLastName()) && getAddress().equals(person.getAddress()) && getGender().equals(person.getGender());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender());
+        }
+}
+
+
